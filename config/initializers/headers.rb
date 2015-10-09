1a64973f6c2f4030158f121470e80a7c55057be9
Steven Davidovitz <sdavidovitz@zendesk.com>
2015-10-09 15:14:52 -0700

    rubocop

--- config/initializers/headers.rb
+++ config/initializers/headers.rb
@@ -1,14 +1,14 @@
 headers = [
-  "HTTP/1.1 200 OK",
-  "Content-Type: text/event-stream",
-  "Cache-Control: no-cache, no-store",
-  "Connection: close"
+  'HTTP/1.1 200 OK',
+  'Content-Type: text/event-stream',
+  'Cache-Control: no-cache, no-store',
+  'Connection: close'
 ]
 
 if origin = SseRailsEngine.access_control_allow_origin
   headers << "Access-Control-Allow-Origin: #{origin}"
 end
 
-headers << "" << ""
+headers << '' << ''
 
 SseRailsEngine::Connection.sse_header = headers.join("\r\n").freeze
